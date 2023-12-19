1ae2b4a1ecf7acaef066a90ca57c4d23bb7c1076
Jaynimi <Eyeslikejays@yahoo.com>
2023-12-19 10:58:16 +0100

    Last Changes

--- resume-builder/src/components/experience.jsx
+++ resume-builder/src/components/experience.jsx
@@ -2,72 +2,72 @@ import React, { useState } from "react";
 import "../App.css";
 
 export default function EducationHeadings({
-	headingText,
-	fontColor,
-	textAlign,
-	width,
-	margin,
-	padding,
-	backgroundColor,
-	display,
-	justifyContent,
-	boxSizing,
-	alignSelf,
-	flexDirection,
-	flexWrap,
-	height,
-	alignItems,
-	fontSize,
-	fontWeight,
+  headingText,
+  fontColor,
+  textAlign,
+  width,
+  margin,
+  padding,
+  backgroundColor,
+  display,
+  justifyContent,
+  boxSizing,
+  alignSelf,
+  flexDirection,
+  flexWrap,
+  height,
+  alignItems,
+  fontSize,
+  fontWeight,
 }) {
-	// Display for Name
-	const resumeInfo = {
-		textAlign: textAlign || "center",
-		// backgroundColor: backgroundColor || "white",
-		color: fontColor || "white",
-		alignSelf: alignSelf || "stretch",
-		fontSize: fontSize || "30px",
-		padding: padding || "2px 0 2px 0",
-		fontWeight: fontWeight || "700",
-		margin: margin || "40px 15% 25px 15%",
-	};
+  // Display for Name
+  const resumeInfo = {
+    textAlign: textAlign || "center",
+    // backgroundColor: backgroundColor || "white",
+    color: fontColor || "white",
+    alignSelf: alignSelf || "stretch",
+    fontSize: fontSize || "30px",
+    padding: padding || "2px 0 2px 0",
+    fontWeight: fontWeight || "700",
+    margin: margin || "40px 15% 25px 15%",
+  };
 
-	// contact info Display
-	const displayContainers = {
-		textAlign: textAlign || "center",
-		width: width || "65%",
-		backgroundColor: backgroundColor || "white",
-		// color: fontColor || "white",
-		flexDirection: flexDirection || "column",
-		display: display || "flex",
-		flexWrap: flexWrap || "wrap",
-	};
+  // contact info Display
+  const displayContainers = {
+    textAlign: textAlign || "center",
+    width: width || "65%",
+    backgroundColor: backgroundColor || "white",
+    // color: fontColor || "white",
+    flexDirection: flexDirection || "column",
+    display: display || "flex",
+    flexWrap: flexWrap || "wrap",
+  };
 
-	const innerContactContainer = {
-		flexDirection: flexDirection || "column",
-		display: display || "flex",
-		width: width || "30%",
-	};
-	const contactContainer = {
-		boxSizing: boxSizing || "border-box",
-		justifyContent: justifyContent || "space-between",
-		display: display || "flex",
-		width: width || "100vw",
-		padding: padding || "0px 30px",
-		flexDirection: flexDirection || "row",
-		alignItems: alignItems || "",
-	};
+  const innerContactContainer = {
+    flexDirection: flexDirection || "column",
+    display: display || "flex",
+    width: width || "30%",
+  };
+  const contactContainer = {
+    boxSizing: boxSizing || "border-box",
+    justifyContent: justifyContent || "space-between",
+    display: display || "flex",
+    width: width || "100vw",
+    padding: padding || "0px 30px",
+    flexDirection: flexDirection || "row",
+    alignItems: alignItems || "",
+  };
 
-	return (
-		<>
-			<div style={contactContainer}>
-				<div style={innerContactContainer}></div>
-				<div style={displayContainers}>
-					<div className="headingText" style={resumeInfo}>
-						{headingText}
-					</div>
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div style={contactContainer}>
+        <div style={innerContactContainer}></div>
+        <div style={displayContainers}>
+          <div className="headingText" style={resumeInfo}>
+            {headingText}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
